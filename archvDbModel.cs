819c7bb24e1557cce8b6a931b33f0b41abfa0848
Pankushg <39335738+Pankushg@users.noreply.github.com>
2018-06-14 01:01:24 +0530

    Add files via upload

---
+++ archvDbModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECCHealthcheck.Models
+{
+    public class archvDbModel
+    {
+        public DateTime archvDttm { get; set; }
+        public int archvCnt { get; set; }
+    }
+}
